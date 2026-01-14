1484be42806dde6e38ddea6f340fc324172e9492
creatorchandu <cpgavhande015@gmail.com>
2026-01-14 14:20:41 +0530

    Add main App component

---
+++ src/src/src/src/App.js
@@ -0,0 +1,29 @@
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Services from "./components/Services";
+import Gallery from "./components/Gallery";
+import Packages from "./components/Packages";
+import WhyChooseUs from "./components/WhyChooseUs";
+import Testimonials from "./components/Testimonials";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <About />
+      <Services />
+      <Gallery />
+      <Packages />
+      <WhyChooseUs />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
